4e3f2da47b7dd48b217343d295bfc50e047cfb95
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2019-06-07 12:52:42 -0400

    initial files

---
+++ bastion.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# adjust these variables
+export SSH_KEY=DevOps
+export CLUSTER_REGION=us-east-1
+export RESOURCE_PREFIX=jyarbrough
+export AMI_ID=ami-07b4156579ea1d7ba
+
+# leave these values as they are
+export AWS_HOST_NAME="$RESOURCE_PREFIX"-dt-kube-demo-bastion
+export AWS_SECURITY_GROUP_NAME="$RESOURCE_PREFIX"-dt-kube-demo-bastion-group
+
+#create default VPC
+aws ec2 create-default-vpc
+
+# create-security-group
+aws ec2 create-security-group \
+  --group-name $AWS_SECURITY_GROUP_NAME \
+  --description "Used by dt-kube-demo bastion host"
+
+# get the new security-group id
+export AWS_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups \
+  --filters "Name=group-name,Values=$AWS_SECURITY_GROUP_NAME" \
+  --query "SecurityGroups[0].GroupId" \
+  --output text)
+
+# update create-security-group with inbound rule
+aws ec2 authorize-security-group-ingress \
+  --group-id "$AWS_SECURITY_GROUP_ID" \
+  --protocol tcp \
+  --port 22 \
+  --cidr "0.0.0.0/0"
+
+# provision the host
+aws ec2 run-instances \
+  --image-id "$AMI_ID" \
+  --count 1 \
+  --security-group-ids "$AWS_SECURITY_GROUP_ID" \
+  --instance-type t2.micro \
+  --key-name $SSH_KEY  \
+  --associate-public-ip-address \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$AWS_HOST_NAME}]" \
+  --region $CLUSTER_REGION
---
+++ removebastion.sh
@@ -0,0 +1,25 @@
+# adjust these variables
+export SSH_KEY=DevOps
+export CLUSTER_REGION=us-east-1
+export RESOURCE_PREFIX=jyarbrough
+
+# leave these values
+export AWS_HOST_NAME="$RESOURCE_PREFIX"-dt-kube-demo-bastion
+export AWS_SECURITY_GROUP_NAME="$RESOURCE_PREFIX"-dt-kube-demo-bastion-group
+
+# get bastion host instance id
+export AWS_INSTANCE_ID=$(aws ec2 describe-instances \
+  --filters "Name=tag:Name,Values=$AWS_HOST_NAME" "Name=instance-state-name,Values=running" \
+  --query "Reservations[0].Instances[0].InstanceId" \
+  --output text)
+# terminate instance
+aws ec2 terminate-instances --instance-ids $AWS_INSTANCE_ID
+
+# get the security-group id
+export AWS_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups \
+  --filters "Name=group-name,Values=$AWS_SECURITY_GROUP_NAME" \
+  --query "SecurityGroups[0].GroupId" \
+  --output text)
+
+# delete the security group
+aws ec2 delete-security-group --group-id $AWS_SECURITY_GROUP_ID
